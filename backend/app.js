26f2a04ea0356e5f916f711d8ac6b762e3b51517
RajKalita234 <rajpratimkalita23@gmail.com>
2022-12-29 13:55:53 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const errorMiddleware = require("./middleware/error")
+
+app.use(express.json())
+// Route Imports
+const productrouter = require("./routes/productRoute");
+
+app.use("/api/v1", productrouter);
+
+// Middleware for Errors
+app.use(errorMiddleware);
+
+module.exports = app
